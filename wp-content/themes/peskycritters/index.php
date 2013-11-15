b64b69c03dd0be32e9b144525095c1b8b2afe837
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-11-15 09:05:24 +0000

    Initial Pesky Critters wordpress project

---
+++ wp-content/themes/peskycritters/index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The main file to display the home page if there is no home.php
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage PeskyCritters
+ * @since Pesky Critters 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endwhile; ?>
+
+			<?php twentythirteen_paging_nav(); ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
